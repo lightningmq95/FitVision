bd5f6b57eb19c88b9cbe7c0fd3983324221b1ee4
Shubham Sarkar <shubham04sarkar@gmail.com>
2025-03-20 09:50:27 +0530

    back

--- FitVisionApp/app/src/main/java/com/fitvision/app/ui/gallery/GalleryFragment.java
+++ FitVisionApp/app/src/main/java/com/fitvision/app/ui/gallery/GalleryFragment.java
@@ -1,18 +1,24 @@
 package com.fitvision.app.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.fitvision.app.R;
 import com.fitvision.app.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
+
+    private static final int PICK_FILE_REQUEST = 1;
 
     private FragmentGalleryBinding binding;
 
@@ -24,9 +30,27 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
+        final TextView textView = binding.textView2;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button uploadButton = binding.upload;
+        uploadButton.setOnClickListener(v -> openFilePicker());
+
+        Button submitButton = binding.submit;
+        submitButton.setOnClickListener(v -> {
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new OutputFragment());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        });
         return root;
+    }
+
+    private void openFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), PICK_FILE_REQUEST);
     }
 
     @Override
---
+++ FitVisionApp/app/src/main/java/com/fitvision/app/ui/gallery/OutputFragment.java
@@ -0,0 +1,22 @@
+// OutputFragment.java
+package com.fitvision.app.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fitvision.app.R;
+
+public class OutputFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_output, container, false);
+    }
+}
