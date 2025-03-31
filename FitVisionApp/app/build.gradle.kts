b5d8cc5eac0d170ea21f6f26721fa94636ad2664
Arsenic <cryoarsenic@gmail.com>
2025-03-31 19:15:34 +0530

    Added grid Ui and dummy api calls

--- FitVisionApp/app/build.gradle.kts
+++ FitVisionApp/app/build.gradle.kts
@@ -59,7 +59,15 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // OK HTTP For making HTTP requests
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+
+    // For parsing JSON
+    implementation ("com.google.code.gson:gson:2.10.1")
+
     // OkHttp for Networking
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
 }
